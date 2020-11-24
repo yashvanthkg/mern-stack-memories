840c0f44a2d9c39159d43c105cb91e5ceb7cd681
yashvanthkg <yashvanth1590@gmail.com>
2020-11-24 15:02:24 +0530

    code refactor

--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const router  = require('./routes/posts')
+const dotenv = require('dotenv');
+const router  = require('./routes/posts');
 
 const app = express();
+dotenv.config();
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use("/posts", router);
 
-const CONNECTION_URL = 'mongodb+srv://mongodb_123:mongodb_123@cluster0.3s2xn.mongodb.net/test?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello , Welcome buudies')
+})
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch(error => console.log(error.message));
 
